fb143edab4524d4dea133d3cd526a75322762ce9
elongva <evl130030@utdallas.edu>
2018-04-24 21:41:09 -0500

    Working version of cdk.cc

--- cdk.cc
+++ cdk.cc
@@ -3,27 +3,55 @@
 
 #include <iostream>
 #include <cdk.h>
-#include <string>
+#include <string.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <iomanip>
 #include <fstream>
 
-#define MATRIX_WIDTH 4
-#define MATRIX_HEIGHT 3
-#define BOX_WIDTH 15
-#define MATRIX_NAME_STRING "Test Matrix"
+#define MATRIX_WIDTH 3
+#define MATRIX_HEIGHT 5
+#define BOX_WIDTH 20
+#define MATRIX_NAME_STRING "Binary File Contents"
 
 using namespace std;
 
+const int maxRecordStringLength = 25;
+
+class BinaryFileHeader{
+public:
+  uint32_t magicNumber;
+  uint32_t versionNumber;
+  uint64_t numRecords;
+};
+
+class BinaryFileRecord{
+public:
+  uint8_t strLength;
+  char stringBuffer[maxRecordStringLength];
+};
+
 int main(){
+  BinaryFileHeader *myHeader = new BinaryFileHeader();
+
+  ifstream binInfile ("cs3377.bin", ios::in | ios::binary);
+
+  binInfile.read((char*) myHeader, sizeof(BinaryFileHeader));
+  cout << "Value was: " << setprecision(10) << myHeader->versionNumber << endl;
+
+  BinaryFileRecord *myRecords = new BinaryFileRecord[myHeader->numRecords]();
+  for(uint32_t i = 0; i < myHeader->numRecords; i++){
+    binInfile.read((char*)(myRecords + i), sizeof(BinaryFileRecord));
+  }
+
+  binInfile.close();
 
   WINDOW *window;
   CDKSCREEN *cdkscreen;
   CDKMATRIX *myMatrix;
 
-  const char *rowTitles[] = {"R0", "R1", "R3", "R4", "R5"};
-  const char *columnTitles[] = {"C0", "C1", "C2", "C3", "C4", "C5"};
+  const char *rowTitles[] = {"R0", "a", "b", "c", "d", "e"};
+  const char *columnTitles[] = {"C0", "a", "b", "c"};
   int boxWidths[] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
   int boxTypes[] = {vMIXED, vMIXED, vMIXED, vMIXED, vMIXED, vMIXED};
 
@@ -46,7 +74,27 @@ int main(){
   drawCDKMatrix(myMatrix, true);
 
   //DISPLAY A MESSAGE
-  setCDKMatrixCell(myMatrix, 2, 2, "Test");
+  //string tempStr = "Magic: 0x" + to_string(myHeader->magicNumber);
+  // setCDKMatrixCell(myMatrix, 1, 1, tempStr.c_str());
+  char buffer[100];
+  sprintf(buffer, "%X", myHeader->magicNumber);
+  string tempStr = "Magic: 0x";
+  tempStr = tempStr + buffer;
+  setCDKMatrixCell(myMatrix, 1, 1, tempStr.c_str());
+
+  tempStr = "Version: ";
+  tempStr = tempStr + to_string(myHeader->versionNumber);
+  setCDKMatrixCell(myMatrix, 1, 2, tempStr.c_str());
+
+  tempStr = "NumRecords: ";
+  tempStr = tempStr + to_string(myHeader->numRecords);
+  setCDKMatrixCell(myMatrix, 1, 3, tempStr.c_str());
+
+  for(uint32_t i = 0; i < myHeader->numRecords; i++){
+    string tempStr = "strlen: " + to_string(static_cast<uint16_t>(myRecords[i].strLength));
+    setCDKMatrixCell(myMatrix, 2 + i, 1, tempStr.c_str());
+    setCDKMatrixCell(myMatrix, 2 + i, 2, myRecords[i].stringBuffer);
+  }
   drawCDKMatrix(myMatrix, true);
   ///////////////////////////////////
   //USED TO PAUSE UNTIL KEY IS PRESSED
